2fe227ec0f785eecdcae2b28eb9380a8780ffba4
jdavidhermoso <jdhermoso@farmadosis.es>
2016-09-13 00:14:37 +0200

    Built the function for validate right hex color, show colors list and hide colors list. Must to refactor some parts, add some css, and go on building some extra functionality. Have to try it with settings passed, too

--- js/jQueryColorPicker.js
+++ js/jQueryColorPicker.js
@@ -1,20 +1,56 @@
 (function($) {
     $.fn.colorPicker = function(options) {
-        var colorPicker = this,
-            validateHexColor = function(color) {
-                var validHexColor =  new RegExp("/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/");
-                if (validHexColor.test(color) ) {
-                    return false;
-                } else {
-                    return true;
-                }
-            };
         this.each(function() {
-            console.log(colorPicker);
+            var settings = $.extend({colorPicker: $(this),defaultColor : '#CCC',colors: [{name:'White',color:'#FFF'},{name:'Silver',color:'#CCC'},{name:'Gray',color:'#888'}],hoverColor: '#3AD'}, options),
+                validateHexColor = function(color) {
+                    var validHexColor =  new RegExp("^#(?:[0-9a-fA-F]{3}){1,2}$");
+                    if (validHexColor.test(color) ) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                showColorsList = function(colors,selectedColor) {
+                    var colorsList = $('<div class="color-picker-container" style="width:'+settings.colorPicker.width()+' ;"></div>'),
+                        color;
+                    for (var i= 0,z = colors.length;i<z;i++) {
+                        color = $('<div class="color" data-color="'+colors[i].color+'" style="background: '+colors[i].color+'">'+colors[i].name+'</div>');
+                        colorsList.append(color);
+                    }
+                    colorsList.insertAfter(settings.colorPicker);
+                };
+                hideColorsList = function() {
+                    settings.colorPicker.next().remove();
+                };
+
+            settings.colorPicker.on('showColorsList',function() {
+                showColorsList(settings.colors);
+            });
+            settings.colorPicker.on('hideColorsList',function() {
+                hideColorsList();
+            });
+            settings.colorPicker.on('selectedColor', function(e,color) {
+                settings.colorPicker.css('background',color);
+                settings.colorPicker.val(color);
+                settings.colorPicker.trigger('hideColorsList');
+            });
+            $(document).on('click','.color', function() {
+                var selectedColor = $(this).data('color');
+                settings.colorPicker.trigger('selectedColor',selectedColor);
+            });
+            $(document).on('mouseover.hover','.color', function() {
+                $(this).css('background',settings.hoverColor);
+            });
+            $(document).on('mouseout.hover','.color', function() {
+                $(this).css('background',$(this).data('color'));
+            });
+
+
            if (validateHexColor($(this).val()) ) {
-               console.log('Color válido');
+               $(this).css('background-color',$(this).val());
            } else {
-               console.log('Invalido');
+               $(this).val(settings.defaultColor);
+               $(this).css('background-color',settings.defaultColor);
            }
         });
     };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.color-picker-input').colorPicker();
+    $('.color-picker-input').on('focus', function() {
+        $('.color-picker-input').trigger('showColorsList');
+    });
+    $('.color-picker-input').on('blur', function() {
+        //$('.color-picker-input').trigger('hideColorsList');
+    });
+
 });
